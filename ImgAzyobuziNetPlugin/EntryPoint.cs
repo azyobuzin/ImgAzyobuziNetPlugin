863a59347c5a05dedf6f43375259a444043f0c11
azyobuzin <azyobuzin@users.sourceforge.jp>
2012-08-31 21:01:36 +0900

    Initial commit

---
+++ ImgAzyobuziNetPlugin/EntryPoint.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.Serialization.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Acuerdo.Plugin;
+using Inscribe.Plugin;
+
+namespace ImgAzyobuziNetPlugin
+{
+    [Export(typeof(IPlugin))]
+    public class EntryPoint : IPlugin
+    {
+        public string Name
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly()
+                    .GetCustomAttributes(false)
+                    .OfType<AssemblyTitleAttribute>()
+                    .Select(a => a.Title)
+                    .FirstOrDefault();
+            }
+        }
+
+        public Version Version
+        {
+            get { return Assembly.GetExecutingAssembly().GetName().Version; }
+        }
+
+        public void Loaded()
+        {
+            UploaderManager.RegisterResolver(new Resolver());
+
+            Task.Factory.StartNew(() =>
+            {
+                using (var wc = new WebClient())
+                using (var reader = JsonReaderWriterFactory.CreateJsonReader(
+                    wc.OpenRead("http://img.azyobuzi.net/api/regex.json"),
+                    XmlDictionaryReaderQuotas.Max))
+                {
+                    Resolver.RegexList.AddRange(
+                        XElement.Load(reader).Elements()
+                            .Select(xe => new Regex(xe.Element("regex").Value, RegexOptions.IgnoreCase))
+                    );
+                }
+            });
+        }
+
+        public IConfigurator ConfigurationInterface
+        {
+            get { return null; }
+        }
+    }
+}
---
+++ ImgAzyobuziNetPlugin/Resolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Acuerdo.External.Uploader;
+
+namespace ImgAzyobuziNetPlugin
+{
+    public class Resolver : IResolver
+    {
+        static Resolver()
+        {
+            RegexList = new List<Regex>();
+        }
+
+        public static List<Regex> RegexList { get; private set; }
+
+        public bool IsResolvable(string url)
+        {
+            return RegexList.Any(r => r.IsMatch(url));
+        }
+
+        public string Resolve(string url)
+        {
+            if (IsResolvable(url))
+                return "http://img.azyobuzi.net/api/redirect?size=large&uri=" + Uri.EscapeDataString(url);
+            else
+                return null;
+        }
+    }
+}
